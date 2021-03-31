cc78dc4e79232b5e9d0b05feb0542e4fffe4490b
mekarls <mekarls@umich.edu>
2021-03-31 16:27:59 -0400

    'problem_with_muting'

--- js/video.js
+++ js/video.js
@@ -1,11 +1,67 @@
-var video;
+var video = document.getElementById('player1');
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML=video.volume * 100 +"%";
+});
 
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log(this.value);
+	var slider = this.value;
+	video.volume = slider/100;
+	document.querySelector("#volume").innerHTML=slider+"%";
+});
+
+
+
+
+
+
+if (video.muted == true) {
+	// console.log(video.muted);
+	document.querySelector('#mute').addEventListener("click", function() {
+		console.log("Unmute Video");
+		video.muted = false;
+		document.querySelector("#mute").innerHTML="Mute";
+})};
+
+if (video.muted == false) {
+	document.querySelector('#mute').addEventListener("click", function() {
+		console.log("Mute Video");
+		video.muted = true;
+		document.querySelector("#mute").innerHTML="Unmute";
+		console.log(video.muted);
+})};
+
+
+document.querySelector('#pause').addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector('#vintage').addEventListener("click", function() {
+	console.log("Vintage");
+	video.classList.add("oldSchool");
+});
+
+document.querySelector('#orig').addEventListener("click", function() {
+	console.log("Original");
+	video.classList.remove("oldSchool");
+});
+
+document.querySelector('#slower').addEventListener("click", function() {
+	console.log("Slow Down");
+	video.playbackRate *= .95;
+	console.log(video.playbackRate);
+});
+
+document.querySelector('#faster').addEventListener("click", function() {
+	console.log("Speed Up");
+	video.playbackRate /= .95;
+	console.log(video.playbackRate);
+});
